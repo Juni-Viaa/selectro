0e598a753993f5636faac657700b28f04bb892d4
muhammad faiz difa suanda <faizsuanda27@gmail.com>
2025-04-06 17:07:51 +0700

    Update routes/web.php sebelum pull

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\EditprodukController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\KeranjangController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/index', [IndexController::class, 'index']);
+Route::get('/editproduk', [EditprodukController::class, 'index']);
+Route::get('/keranjang', [KeranjangController::class, 'index']);
+
